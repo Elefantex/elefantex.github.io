84dcc5f9140be487db655e674f91634a04abd894
Elefantex <samuel_borque@hotmail.com>
2023-06-16 17:01:40 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -6,62 +6,62 @@ function App() {
   const cartas = [
     {
       titulo: "9 Segundos",
-      descripcion: "Empezando con las manos en la espalda,debes tardar menos de 9 segundos en sacar una ficha de la torre y colocarla en la cima",
+      descripcion: "Empezando con las manos en la espalda,debes tardar menos de <b> 9 segundos</b>  en sacar una ficha de la torre y colocarla en la cima",
       imagen: "https://i.imgur.com/wjbYGNv1.jpg"
     },
     {
       titulo: "A ciegas",
-      descripcion: "Saca una ficha con los ojos cerrados siguiendo las indicaciones un jugar elegido por ti. Si se cae la torre perdeis los dos. Una vez sacada la ficha, abre los ojos y colocala",
+      descripcion: "<b> Saca una ficha con losojos cerrados </b> siguiendo las indicaciones un jugar elegido por ti. Si se cae la torre perdeis los dos. <b> Una vez sacada la ficha, abre los ojos y colocala </b>",
       imagen: "https://i.imgur.com/wjbYGNv2.jpg"
     },
     {
       titulo: "A ciegas completo",
-      descripcion: "Saca una ficha con los ojos cerrados siguiendo las indicaciones un jugar elegido por ti y colocala en la torre con los ojos cerrados. Si se cae la torre perdeis los dos.",
+      descripcion: "<b>  Saca una ficha con los ojos cerrados</b>   siguiendo las indicaciones un jugar elegido por ti y <b> colocala en la torre con los ojos cerrados </b> . Si se cae la torre perdeis los dos.",
       imagen: "https://i.imgur.com/wjbYGNv3.jpg"
     },
     {
       titulo: "Indices amigos",
-      descripcion: "Elige a un compañero, debereis sacar y colocar en la cima una ficha entre los dos y cada uno solo podra un dedo indice, si se cae la torre perdeis los dos",
+      descripcion: "Elige a un compañero,<b>  debereis sacar y colocar en la cima una ficha entre los dos y cada uno solo podra un dedo indice </b>, si se cae la torre perdeis los dos",
       imagen: "https://i.imgur.com/wjbYGNv4.jpg"
     },
     {
       titulo: "Dos meñiques",
-      descripcion: "Utiliza solo los dos dedos meñiques para sacar una ficha y colocarla en la cima. Si apoyas la ficha en otro sitio que no sea la cima o se te cae no habras superado la prueba",
+      descripcion: "Utiliza solo <b>  los dos dedos meñiques para sacar una ficha y colocarla en la cima </b>. Si apoyas la ficha en otro sitio que no sea la cima o se te cae no habras superado la prueba",
       imagen: "https://i.imgur.com/wjbYGNv5.jpg"
     },
     {
       titulo: "5 Segundos",
-      descripcion: "Empezando con las manos en la espalda,debes tardar menos de 5 segundos en sacar una ficha de la torre y colocarla en la cima",
+      descripcion: "Empezando con las manos en la espalda,debes tardar menos de<b>  5 segundos</b> en sacar una ficha de la torre y colocarla en la cima",
       imagen: "https://i.imgur.com/wjbYGNv6.jpg"
     },
     {
       titulo: "Constructor",
-      descripcion: "Coge una ficha de la cimda y colocara en un huevo que haya en la torre",
+      descripcion: "<b> Coge una ficha de la cimda y colocara en un huevo que haya en la torre </b>",
       imagen: "https://i.imgur.com/wjbYGNv7.jpg"
     },
     {
       titulo: "Mano Mala",
-      descripcion: "Utiliza la mano no dominante para sacar una ficha y colocarla en la cima",
+      descripcion: "Utiliza <b>  la mano no dominante </b> para sacar una ficha y colocarla en la cima",
       imagen: "https://i.imgur.com/wjbYGNv8.jpg"
     },
     {
       titulo: "A soplar",
-      descripcion: "Sopla a la torre mientras sacas una ficha",
+      descripcion: "<b> Sopla a la torre mientras sacas una ficha </b>",
       imagen: "https://i.imgur.com/wjbYGNv9.jpg"
     },
     {
       titulo: "El izquierdo molesto",
-      descripcion: "Mientras sacas una ficha y la colocas, el jugador de tu izquierda puede molestarte",
+      descripcion: "Mientras sacas una ficha y la colocas,<b>  el jugador de tu izquierda puede molestarte </b>",
       imagen: "https://i.imgur.com/wjbYGNv10.jpg"
     },
     {
       titulo: "El derecho molesto",
-      descripcion: "Mientras sacas una ficha y la colocas, el jugador de tu derecha puede molestarte",
+      descripcion: "Mientras sacas una ficha y la colocas,<b>  el jugador de tu derecha puede molestarte </b>",
       imagen: "https://i.imgur.com/wjbYGNv11.jpg"
     },
     {
       titulo: "Comodin",
-      descripcion: "Juega normal",
+      descripcion: "<b> Juega normal </b>",
       imagen: "https://i.imgur.com/wjbYGNv12.jpg"
     },
   ];
@@ -109,10 +109,10 @@ function App() {
             <h2 style={{ marginLeft: "10px" }}>
               {cartas.find(carta => carta.imagen === backgroundImage)?.titulo}
             </h2>
-            <div style={{ marginLeft: "10px" }}>
-              {cartas.find(carta => carta.imagen === backgroundImage)?.descripcion}
-
-            </div>
+            <div style={{ marginLeft: '10px' }}>
+          {/* Utiliza dangerouslySetInnerHTML para renderizar HTML */}
+          <span dangerouslySetInnerHTML={{ __html: cartas.find(carta => carta.imagen === backgroundImage)?.descripcion }} />
+        </div>
           </div>
 
         </FlippingCardBack>
